65e34611307f9a3751b71a5213c2928154f7087c
AnasNazar880 <102611445+AnasNazar880@users.noreply.github.com>
2023-09-21 21:10:32 +0530

    commit

--- src/test/java/com.orangehrm/testscripts/ResetPageTest.java
+++ src/test/java/com.orangehrm/testscripts/ResetPageTest.java
@@ -1,6 +1,9 @@
 package com.orangehrm.testscripts;
 
+import org.testng.annotations.Test;
+
 public class ResetPageTest {
+    @Test
     public void printMyName(){
         System.out.println("i am izin");
     }
